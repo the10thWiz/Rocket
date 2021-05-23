968c096ccc31d70f2065c35934edd358220eca8a
Matthew Pomes <the10thwiz@gmail.com>
2021-05-22 20:16:16 -0500

    Switch to defaults

--- core/lib/tests/http_uri_serde.rs
+++ core/lib/tests/http_uri_serde.rs
@@ -52,7 +52,7 @@ fn uri_serde() {
 
 #[test]
 fn uri_serde_round_trip() {
-    let tmp = Figment::from(Serialized::default("uris", UriContainer {
+    let tmp = Figment::from(Serialized::defaults(UriContainer {
         asterisk: Asterisk,
         origin: uri!("/foo/bar?baz"),
         authority: uri!("user:pass@rocket.rs:80"),
@@ -60,7 +60,7 @@ fn uri_serde_round_trip() {
         reference: uri!("https://rocket.rs:8000/index.html").into(),
     }));
 
-    let uris: UriContainer<'_> = tmp.extract_inner("uris").expect("Parsing failed");
+    let uris: UriContainer<'_> = tmp.extract().expect("Parsing failed");
     assert_eq!(uris, UriContainer {
         asterisk: Asterisk,
         origin: uri!("/foo/bar?baz"),
