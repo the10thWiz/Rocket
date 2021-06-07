0e59bb1681dc786ce305b94a09665ca25ba6b440
Matthew Pomes <the10thwiz@gmail.com>
2021-06-07 13:24:13 -0500

    Imporve protocol specification

--- core/lib/src/channels/mod.rs
+++ core/lib/src/channels/mod.rs
@@ -79,7 +79,7 @@
 //!
 //! - [ ] Better panic handling: At the moment, nothing is reported to the client, and the
 //! connection continues as normal
-//! - [ ] Identify potential panics
+//! - [ ] Identify potential panics in Rocket
 //! - [ ] Full Autobahn compliance: Potential exceptions are possible, since Rocket is very async
 //! & very fast
 //! - [ ] Topic multiplexing; rocket-multiplex
@@ -88,6 +88,7 @@
 //! - [ ] Broker improvements
 //! - [ ] Finalize data portion of the attribute macros
 //! - [ ] Finalize the `IntoMessage` implementations
+//! - [ ] Subprotocol support. Depends on rocket-multiplex support for subprotocols
 //!
 //! ## Nice to haves
 //!
@@ -107,22 +108,21 @@
 //! back. This means that pongs often don't arrive in the order Autobahn expects, and Rocket can
 //! start echoing a message before it has finished recieving it.
 //!
-//! - [5.19, 5.20] sometimes fail to send pongs & message in the correct order - this is likely a data
+//! - [ ] (5.19, 5.20) sometimes fail to send pongs & message in the correct order - this is likely a data
 //!   race between reading and replying - it starts replying right away, before it's finished reading
 //!   the frame
 //!     Interestingly, the `Case Expectation` provided by Autobahn seems to indicate that our
 //!     behaviour is acceptable. Case 5.20 notes that 5.19 and 5.20 should behave the same, but
 //!     this is not always the case. Based on some peliminary testing, is looks like 5.20 always
-//!     fails, but 5.19 occationally passes. The only difference between the two cases is that 5.20
-//!     splits the messages up and sends them byte by byte, which reinforces the idea that this is
-//!     a race, and the rate of transmission effects the outcome.
-//! - [7.1.5] fails since Rocket will forward part of a message back. This is because Rocket will
+//!     fails, but 5.19 occasionally passes. The only difference between the two cases is that 5.20
+//!     splits the messages up and sends them byte by byte.
+//! - [ ] (7.1.5) fails since Rocket will forward part of a message back. This is because Rocket will
 //!     consider the frame to have ended, despite the fact that it didn't. I don't think there is a
 //!     way to handle this correctly using the current model, since the server side handler is
 //!     already running by the time the error happens.
 //!
-//! - [6.*] fail since the echo server doesn't check UTF-8
-//! - [12.*, 13.*] test compression, which we don't implement (yet)
+//! - [ ] (6.*) fail since the echo server doesn't check UTF-8
+//! - [ ] (12.*, 13.*) test compression, which we don't implement (yet)
 //!
 //! There are also a number of informational tests, which don't have correct behaviour specified by
 //! the RFC, but we should look at them and decide whether our current behaviour is good enough
@@ -155,9 +155,47 @@ pub mod rocket_multiplex {
     //! Rocket uses the Origin URL of a websocket request as a topic identifier. The rocket-multiplex
     //! proprotocol allows sending messages to multiple topics using a single websocket connection.
     //!
-    //! Topic URLS are limited to `MAX_TOPIC_LENGTH = 100`, to prevent potential DoS attacks.
+    //! # Design considerations
+    //!
+    //! - [ ] Simple
+    //! - [ ] RFC compliant
+    //! - [ ] Transparent
+    //!
+    //! One major goal is to make this protocol transparent: on Rocket's side, user code cannot
+    //! tell whether the connection is multiplexed or not. There may be some differences, such as
+    //! a multiplexed connection only counting towards a connection limit as a single connection.
+    //! It should also be possible to design client libraries that are transparent, where user code
+    //! can't tell whether the connections are being multiplexed or not.
+    //!
+    //! # Handshake
+    //!
+    //! rocket-multiplex acts as a subprotocol for websocket connections, like json or xml might
+    //! be. Most websocket libraries should provide functionality to request a subprotocol, and
+    //! should therefore be able to complete the handshake. Use `'rocket-multiplex'`, case
+    //! insesitively.
+    //!
+    //! ## Subprotocols
+    //!
+    //! As currently defined, there is no way to specify another subprotocol in addition to
+    //! rocket-multiplex.
+    //!
+    //! One option is to specify `rocket-multiplex-*`, where `*` is a subprotocol, and perhaps add
+    //! it to the subscribe action. This would require updating and improving the error responses,
+    //! which should probably be done anyway.
     //!
     //! # Messages: Data & Control
+    //!
+    //! Notes:
+    //!
+    //! This protocol doesn't specify how the messages should be transimitted, and Rocket allows
+    //! anything the RFC considers legal. For example, there is no requirement that the control
+    //! portions of the message are sent as a single frame.
+    //!
+    //! ### Seperator character: `'\u{00B7}'`
+    //!
+    //! This character was chosen because it is not a valid character in a URL (according to the
+    //! spec, if MUST be percent encoded), and it is valid UTF-8, i.e. it can be included in a Text
+    //! message.
     //!
     //! ## Data
     //!
@@ -168,10 +206,12 @@ pub mod rocket_multiplex {
     //! Data messages sent to a topic the client is not subscribed to result in an error being sent to
     //! the client.
     //!
+    //! Topic URLS are limited to `MAX_TOPIC_LENGTH = 100` (in bytes), to prevent potential DoS attacks.
+    //!
     //! # Control
     //!
     //! Control messages are limited to 512 bytes total, although there should never be a reason to
-    //! create a longer message. Control messages should be marked as text and take the form:
+    //! create a longer message. Control messages must be marked as text and take the form:
     //!
     //! `S ACTION S (PARAM S)*`
     //!
@@ -182,18 +222,60 @@ pub mod rocket_multiplex {
     //!
     //! - Subscribe: `SUBSCRIBE`, [Topic]; subscribed the client to a specific topic URL, as if the
     //! client had opened a second websocket connection to the topic URL
-    //! - Unsubscribe: `UNSUBSCRIBE`, [TOPIC]; unsubscribes the client from a specific topic URL, as if
-    //! the client has closed the second websocket connection to the topic URL
+    //! - Unsubscribe: `UNSUBSCRIBE`, [TOPIC, CODE?, REASON?]; unsubscribes the client from a specific
+    //! topic URL, as if the client has closed the second websocket connection to the topic URL
     //! - Unsubscribe all: There is no specific unsubscribe all action, although closing the websocket
     //! connection is treated as an unsubscribe all
     //!
-    //! - Ok: `OK`, []; Sent as a response to an action, this indicates that the action
+    //! - Ok: `OK`, [ACTION, PARAMS*]; Sent as a response to an action, this indicates that the action
     //! succeeded.
-    //! - Err: `ERR`, [ACTION, PARAMS]; Sent as a response to an action, this indicates that the action
-    //! failed.
+    //! - Err: `ERR`, [REASON, ACTION, PARAMS*]; Sent as a response to an action, this indicates that
+    //! the action failed. REASON is case-insesitive.
     //! - Invalid message: `INVALID`, [REASON]; Sent as a response to an message the client is not
     //! allowed to send. Currently, this is only sent in response to a message to a topic the client is
     //! not subscribed to.
+    //!
+    //! ## Defined errors
+    //!
+    //! The following errors are defined as part of this protocol. The server implementation can
+    //! return errors not on this list, but should prefer to return one of the errors on this list
+    //! if applicable.
+    //!
+    //! ### Subscribe errors
+    //!
+    //! - Too many topics: indicates that the client has subscribed to too many topics on this
+    //! connection. The limit is not defined by this spec, and no guarentees are made about the
+    //! server's behaviour when returning this error. For example, the server is allowed to change
+    //! the limit on the fly, or anything else. The server isn't allowed to drop existing
+    //! subscriptions, except by termintating the connection as a whole.
+    //!
+    //! - Not found: Inicates that the requested topic doesn't exist. This is equavalent to the
+    //! server returning a 404 during the opening handshake.
+    //!
+    //! ### Unsubscribe errors
+    //!
+    //! - Not subscribed: Inicates that the client was not subscribed to this topic, and therefore
+    //! cannot be unsubscribed.
+    //!
+    //! ### Websocket errors
+    //!
+    //! When a websocket connection encounters an error defined by the RFC, the server is permitted
+    //! to close the connection with the error
+    //!
+    //! # Limits
+    //!
+    //! As noted above, this protocol does not specify many limits. Implementations are free to
+    //! add limits as needed, and should try to return inforative errors when possible.
+    //!
+    //! # Stability & Recovery
+    //!
+    //! This spec makes no guarentees about the stability of the connection. There is also no
+    //! specific mechanism to resubscribe clients to topics they were subscribed to before they
+    //! were disconnected. Rather, clients are responsible for resubscribing to topics they wish to
+    //! resubscribe to.
+    //!
+    //! There is also no builtin mechanism for message persistance, this should be implemented
+    //! seperatly. This can take the form of a HTTP route to get previous messages.
 
     /// Maximum length of topic URLs, with the possible exception of the original URL used to connect.
     ///
@@ -207,5 +289,6 @@ pub mod rocket_multiplex {
     /// there is no ambiguity between a URL containing this character, and a URL terminated by this
     /// character
     pub const MULTIPLEX_CONTROL_STR: &'static str = "\u{B7}";
+    /// `MULTIPLEX_CONTROL_STR`, but as a `&'static [u8]`
     pub const MULTIPLEX_CONTROL_CHAR: &'static [u8] = MULTIPLEX_CONTROL_STR.as_bytes();
 }
