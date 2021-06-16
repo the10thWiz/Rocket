39ea958e9aeb188928b1be70d246edd0102afb7b
Matthew Pomes <the10thwiz@gmail.com>
2021-06-16 09:56:51 -0500

    Add websocket example

---
+++ examples/websocket-echo/src/main.rs
@@ -0,0 +1,74 @@
+#[macro_use] extern crate rocket;
+
+use rocket::channels::Channel;
+use rocket::response::content::Html;
+use rocket::Data;
+
+#[message("/echo", data = "<data>")]
+async fn echo(data: Data, websocket: Channel<'_>) {
+    websocket.send(data).await;
+}
+
+#[get("/")]
+fn index() -> Html<&'static str> {
+    Html(r#"<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>WebSocket Echo Server</title>
+    </head>
+    <body>
+        <h1>Echo Server</h1>
+        <p id="status">Connecting...</p>
+        <input type="text" id="text" />
+        <button type="button" id="send">Send</button>
+        <div id="lines"></div>
+        <script type="text/javascript">
+            const lines = document.getElementById('lines');
+            const text = document.getElementById('text');
+            const status = document.getElementById('status');
+            const ws = new WebSocket('ws://' + location.host + '/echo');
+            ws.onopen = function(e) {
+                status.innerText = 'Connected :)';
+                console.log(e);
+            };
+            ws.onclose = function(e) {
+                status.innerText = 'Disconnected :(';
+                lines.innerHTML = '';
+                console.log(e);
+            };
+            ws.onmessage = function(msg, e) {
+                const line = document.createElement('p');
+                line.innerText = msg.data;
+                lines.prepend(line);
+                console.log(e);
+            };
+            send.onclick = function(e) {
+                ws.send(text.value);
+                text.value = '';
+                console.log(e);
+            };
+            // JS code to trigger race condition in webscoket implementation
+            var arr = [];
+            for(let i = 0; i < 100; i++) {
+                arr.push(i);
+            }
+            function trigger_race() {
+                for(let i = 0; i < 10; i++) {
+                    window.setTimeout(() => {
+                        ws.send(JSON.stringify(arr));
+                    }, i * 10);
+                }
+            }
+            let tmp = JSON.stringify(arr) + '\n';
+            for(let i = 0; i < 5; i++) {
+                tmp+= tmp;
+            }
+        </script>
+    </body>
+</html>"#)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![echo, index])
+}
